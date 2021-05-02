53fb6b8543903fcad18b7ec91e5e52ea9faa2afd
nesezon <tilichenko@gmail.com>
2021-05-02 20:29:39 +0300

    Урок 3. Пустой проект

---
+++ 3 CodeFirst1toN/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst1toN {
+  class Program {
+    static void Main(string[] args) {
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ 3 CodeFirst1toN/README.md
@@ -0,0 +1,2 @@
+C# Advanced. Homework 05. Task 3
+## CodeFirst1toN
