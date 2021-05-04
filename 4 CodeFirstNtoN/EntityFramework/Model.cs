b7af0b8c6f002ae573a6b98e851afa5435658b46
nesezon <tilichenko@gmail.com>
2021-05-04 23:22:05 +0300

    Задание 4. +Модель и строка подключения

---
+++ 4 CodeFirstNtoN/EntityFramework/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace CodeFirstNtoN.EntityFramework {
+  public class Model : DbContext {
+    public Model() : base("name=Model") { }
+
+    // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+    // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+    // public virtual DbSet<MyEntity> MyEntities { get; set; }
+  }
+
+  //public class MyEntity
+  //{
+  //    public int Id { get; set; }
+  //    public string Name { get; set; }
+  //}
+}
