cfbf97745a2d0a2ad8534de6ce4e82b8909ad1f0
nesezon <tilichenko@gmail.com>
2021-05-06 22:34:21 +0300

    Задание 6. Пустая модель

---
+++ 6 CodeFirst1toNOperations/EntityFramework/Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace CodeFirst1toNOperations.EntityFramework {
+  public class Model : DbContext {
+    // Контекст настроен для использования строки подключения "Model" из файла конфигурации  
+    // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+    // "CodeFirst1toNOperations.EntityFramework.Model" в экземпляре LocalDb. 
+    // 
+    // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "Model" 
+    // в файле конфигурации приложения.
+    public Model()
+        : base("name=Model") {
+    }
+
+    // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+    // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+    // public virtual DbSet<MyEntity> MyEntities { get; set; }
+  }
+
+  //public class MyEntity
+  //{
+  //    public int Id { get; set; }
+  //    public string Name { get; set; }
+  //}
+}
---
+++ 6 CodeFirst1toNOperations/README.md
@@ -0,0 +1,6 @@
+﻿C# Advanced. Homework 05. Task 6
+## CodeFirst1toNOperations
+
+Строка подключения в файле [App.config](https://github.com/nesezon/C--advanced-homework-05/blob/main/6%20CodeFirst1toNOperations/App.config). В ней необходимо исправить IP сервера, логин и пароль.
+
+Скрипты автоматически созданных таблиц можно посмотреть в файле [EntityFramework/FinalDB.sql](https://github.com/nesezon/C--advanced-homework-05/blob/main/6%20CodeFirst1toNOperations/EntityFramework/FinalDB.sql)
